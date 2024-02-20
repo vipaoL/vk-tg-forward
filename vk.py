f215615ed14e19a71a40e16901b99b6b117c0b82
vipoll <vipollmail@gmail.com>
2024-02-20 12:30:31 +0400

    add support for forwarded messages

--- vk.py
+++ vk.py
@@ -4,6 +4,7 @@ import traceback
 import vkbottle
 from vkbottle import Bot
 from vkbottle.bot import Message
+from vkbottle.tools.dev.mini_types.bot.foreign_message import ForeignMessageMin
 from vkbottle_types.codegen.objects import UsersUserFull
 from vkbottle_types.objects import MessagesMessageAttachmentType, MessagesMessageAttachment
 
@@ -37,26 +38,7 @@ class VkListenerBot:
             else:
                 to_tg_id = TgBot.TG_ADMIN_CHAT_ID
                 text += "From chat id" + str(message.chat_id) + ": "
-            try:
-                text += message.text
-                sender_id = message.from_id
-                user: UsersUserFull = (await self.vk.api.users.get(user_ids=[sender_id], fields=["screen_name"]))[0]
-                sender_name: str = user.first_name + " " + user.last_name
-                text = sender_name + ": " + text
-                attachments = message.attachments
-                if attachments is not None and len(attachments) > 0:
-                    text = "[" + str(len(attachments)) + " attachments] " + text
-                try:
-                    for a in attachments:
-                        text += self.handle_attachment(a, to_tg_id)
-                except Exception as err_iterating_attachments:
-                    text += " [Error while iterating attachments]"
-                    print(err_iterating_attachments)
-            except Exception as ex:
-                text += " [Error while fetching more info]: " + str(ex)
-                traceback.print_tb(ex.__traceback__)
-            print(message.chat_id, text)
-            self.tg_bot.send_text(text, to_tg_id, disable_notification=False, enable_md=False)
+            await self.handle_message(text, message, to_tg_id)
 
         while not self.is_stopped:
             print("starting vk polling")
@@ -68,6 +50,42 @@ class VkListenerBot:
             if not self.is_stopped:
                 time.sleep(5)
         print("VkListenerBot: stopped")
+
+    async def handle_message(self, base_text: str, message: vkbottle.bot.Message, to_tg_id: int):
+        base_text = base_text
+        try:
+            base_text += message.text
+            sender_id = message.from_id
+            user: UsersUserFull = (await self.vk.api.users.get(user_ids=[sender_id], fields=["screen_name"]))[0]
+            sender_name: str = user.first_name + " " + user.last_name
+            base_text = sender_name + ": " + base_text
+            base_text += self.handle_attachments(message.attachments, to_tg_id)
+            base_text += (await self.handle_forwarded(message.fwd_messages, to_tg_id))
+        except Exception as ex:
+            base_text += " [Error while fetching more info]: " + str(ex)
+            traceback.print_tb(ex.__traceback__)
+        print(message.chat_id, base_text)
+        self.tg_bot.send_text(base_text, to_tg_id, disable_notification=False, enable_md=False)
+
+    async def handle_forwarded(self, forwarded: list[ForeignMessageMin], to_tg_id: int) -> str:
+        if forwarded == None or len(forwarded) < 1:
+            return ""
+        for fwd_message in forwarded:
+            await self.handle_message("↩️ ", fwd_message, to_tg_id)
+
+        return str(" [" + str(len(forwarded)) + " forwarded]")
+
+    def handle_attachments(self, attachments: list[MessagesMessageAttachment], to_tg_id: int) -> str:
+        text = ""
+        if attachments is not None and len(attachments) > 0:
+            text += "[" + str(len(attachments)) + " attachments] "
+        try:
+            for a in attachments:
+                text += self.handle_attachment(a, to_tg_id)
+        except Exception as err_iterating_attachments:
+            text += " [Error while iterating attachments]"
+            print(err_iterating_attachments)
+        return text
 
     def handle_attachment(self, a: MessagesMessageAttachment, to_tg_id: int) -> str:
         if a.type == MessagesMessageAttachmentType.PHOTO:
